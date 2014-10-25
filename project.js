cebcbbeb859e8b476d6b5d2cbe8db52627eac518
pkjvish <vishpkj@gmail.com>
2014-10-25 20:30:29 +0530

    update
    update

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 angular.module('project', ['ngRoute', 'firebase'])
  
-.value('fbURL', 'https://angularjs-projects.firebaseio.com/')
+.value('fbURL', 'https://angularjs-projects.firebaseio.com/' )
  
 .factory('Projects', function($firebase, fbURL) {
   return $firebase(new Firebase(fbURL)).$asArray();
